e2a94ce03af18876d25a77093840ad33817518cb
Soria <soriasayer@gmail.com>
2020-12-13 13:38:17 +0100

    Changed the message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'})
+  res.send({hi: 'Love you dear'})
 })
 
 const PORT = process.env.PORT || 5000

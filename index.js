ab68563eaaae518b7a99d032fb68a2fb8063ac3a
Soria <soriasayer@gmail.com>
2020-12-18 18:42:19 +0100

    Enabling Cookies.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 require('./models/User')
 require('./services/passport')
 
@@ -11,6 +13,17 @@ connection.once('open',()=>{
 })
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000

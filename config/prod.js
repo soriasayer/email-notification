0cb2b1f7aaaea5f0a79f97802637c8a83499c7a7
Soria <soriasayer@gmail.com>
2020-12-19 21:35:52 +0100

    Creatign production environment to the applicatin.

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.KOOKIE_KEY
+}
+
+
+// 'mongodb+srv://ashkaan:wTTwonFe9XZCHdsA@cluster0.ex4mb.mongodb.net/<production>?retryWrites=true&w=majority'

c14189d9a35cddb6309ba6883819d06816fe9b10
Soria <soriasayer@gmail.com>
2020-12-26 11:00:53 +0100

    Creating action to send user token to the sever side of application.

--- client/src/component/Payments.js
+++ client/src/component/Payments.js
@@ -5,10 +5,14 @@ const Payments = () => {
 
   return (
     <StripeChckout
+      name='Feedback collection'
+      description='$5 for 5 email credits'
       amount={500}
       token={token => console.log(token)}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
-    />
+    >
+      <button className='btn'>Add Credits</button>
+    </StripeChckout>
   )
 }
 

9d2a4c67bcbd055274255d821e1a6840563a2b30
Soria <soriasayer@gmail.com>
2021-01-01 17:37:03 +0100

    Creating SurveyForm component.

--- client/src/component/surveys/SurveyForm.js
+++ client/src/component/surveys/SurveyForm.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const SurveyForm = () => {
+const SurveyForm = ({handleSubmit}) => {
   return (
     <div>
-      survey form
+      <form onSubmit={handleSubmit(values => console.log(values)) }>
+        <Field
+          type='text'
+          name='surveyTitle'
+          component='input'
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
 
-export default SurveyForm
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm)

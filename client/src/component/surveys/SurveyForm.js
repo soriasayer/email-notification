a537695d0bb542ea3e7f024172778360180ca6dd
Soria <soriasayer@gmail.com>
2021-01-02 12:53:04 +0100

    Creating survey fields for diffirent inputs.

--- client/src/component/surveys/SurveyForm.js
+++ client/src/component/surveys/SurveyForm.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import SurveyField from './SurveyField'
 
 const SurveyForm = ({handleSubmit}) => {
+  const fields = [
+    {label: 'Survey Title', name: 'title'},
+    {label: 'Subject Line', name: 'subject'},
+    {label: 'Email Body', name: 'body'},
+    {label: 'Recipient List', name: 'emails'}
+  ]
+
+  const renderFields = () => (
+    <div>
+      {fields.map(({label, name}) => (
+        <Field
+          key={name}
+          label={label}
+          type='text'
+          name={name}
+          component={SurveyField}
+        />
+      ))}
+    </div>
+  )
+
   return (
     <div>
       <form onSubmit={handleSubmit(values => console.log(values)) }>
-        <Field
-          type='text'
-          name='surveyTitle'
-          component='input'
-        />
+        {renderFields()}
         <button type='submit'>Submit</button>
       </form>
     </div>

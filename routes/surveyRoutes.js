993bef0898618009bf5378810e15fa6cc3c35732
Soria <soriasayer@gmail.com>
2021-01-04 10:28:19 +0100

    Extracting the url and user email form webhooks object.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,6 +3,9 @@ const requireLogin = require( "../middlewares/requireLogin" )
 const requiredCredits = require('../middlewares/requiredCredits')
 const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+const _ = require('lodash')
+const {URL} = require('url')
+const { Path } = require( 'path-parser' )
 
 const Survey = mongoose.model('surveys')
 
@@ -10,9 +13,29 @@ module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!')
   })
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const events = _.map(req.body, ({email, url}) => {
+      const pathname = new URL(url).pathname
+      const p = new Path('/api/surveys/:surveyId/:choice')
+      const match = p.test(pathname)
+      if(match) {
+        const {surveyId, choice} = match
+        return {email, surveyId, choice}
+      }
+    })
+
+    // removes all undefinds
+    const compactEvents = _.compact(events)
+    // extract uniq elements
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId')
+    console.log(uniqueEvents)
+    res.send({})
+  })
+
   app.post('/api/surveys', requireLogin, requiredCredits, async(req, res) => {
     const {title, subject, body, recipients} = req.body
-    console.log(req.body, recipients.split(','))
+
     const survey = new Survey({
       title,
       subject,
